4ac00b9fba73b0632fa328da9fc9383b2d6bde90
Koutaro Mukai <mukai.k1011k@outlook.jp>
2023-02-12 12:50:54 +0900

    Add download scripts

--- link.bat
+++ link.bat
@@ -1,3 +1,5 @@
 mklink %USERPROFILE%"\_vimrc" %USERPROFILE%"\dotfiles\.vimrc"
 mklink %USERPROFILE%"\_gvimrc" %USERPROFILE%"\dotfiles\.gvimrc"
 
+curl -fLo %USERPROFILE%"\vimfiles\colors\despacio.vim" --create-dirs https://raw.githubusercontent.com/AlessandroYorba/Despacio/master/colors/despacio.vim
+curl -fLo %USERPROFILE%"\vimfiles\autoload\plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- link.sh
+++ link.sh
@@ -2,3 +2,5 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 
+curl -fLo ~/.vim/colors/despacio.vim --create-dirs https://raw.githubusercontent.com/AlessandroYorba/Despacio/master/colors/despacio.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
